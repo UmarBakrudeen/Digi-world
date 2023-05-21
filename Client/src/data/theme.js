8628e74207583c398bff6b5c24ca6bec28ba66a2
UmarBakrudeen <thalaivaumar9501.s@gmail.com>
2023-05-21 10:30:43 +0530

    Initial Commit

---
+++ Client/src/data/theme.js
@@ -0,0 +1,13 @@
+export default {
+  name: "souqarenas",
+  fullName: "Ecommere plateform",
+  author: {
+    name: "souqarenas",
+    profile_url: "https://wwww.souqarenas.com",
+  },
+  contacts: {
+    address: "souqarenas Pvt. Ltd.",
+    email: "support@souqarenas.com",
+    phone: "+91 9237857155",
+  },
+};
